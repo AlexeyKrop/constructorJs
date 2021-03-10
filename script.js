af2e1f18da01afdbf2eede7a93e6475e038ace56
AlexeyKrop <65413959+AlexeyKrop@users.noreply.github.com>
2021-03-10 15:57:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,38 @@ function DomElement(selector, height, width, bg, fontSize) {
   this.bg = bg;
   this.fontSize = fontSize;
   DomElement.prototype.element = function(){
-
+    let count = 10;
     if(selector === '.block'){
-      let div = document.createElement('div');
-      div.classList.add('block');
-      document.body.append(div);
-      div.style.height = height;
-      div.style.width = width;
-      div.style.background = bg;
-      div.style.fontSize = fontSize;
-      div.innerHTML = 'Привет';
+      let block = document.createElement('div');
+      block.classList.add('block');
+      document.body.append(block);
+      block.style.height = height;
+      block.style.width = width;
+      block.style.background = bg;
+      block.style.fontSize = fontSize;
+      block.style.position = 'absolute';
+      block.innerHTML = 'Привет';
+      document.addEventListener('keydown', function(e) {
+        const key = e.key;
+        if(key === 'ArrowRight'){
+          count += 10;
+          block.style.background = 'green';
+          block.style.left = count + 'px';
+        }
+        else if(key === 'ArrowLeft'){
+          count -= 10;
+          block.style.background = 'blue';
+          block.style.left = count + 'px';
+        } else if(key === 'ArrowDown'){
+          count += 10;
+          block.style.background = 'yellow';
+          block.style.top = count + 'px';
+        }else if(key === 'ArrowUp'){
+          count -= 10;
+          block.style.background = 'grey';
+          block.style.top = count + 'px';
+        }
+      })
     }else if(selector === '#best'){
        let paragraph = document.createElement('p');
        paragraph.id = '#best';
