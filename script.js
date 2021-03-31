303834d3b2ed01f297d20a434da921eccfcec66e
AlexeyKrop <65413959+AlexeyKrop@users.noreply.github.com>
2021-03-31 13:50:46 +0300

    Исправил

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
-'use strict';
+"use strict";
 function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector; 
+  this.selector = selector;
   this.height = height;
-  this.width = width; 
+  this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
-  DomElement.prototype.element = function(){
-    let count = 10;
-    if(selector === '.block'){
-      let block = document.createElement('div');
-      block.classList.add('block');
+  DomElement.prototype.element = function () {
+    let count = 0;
+    if (selector === ".block") {
+      let block = document.createElement("div");
+      block.classList.add("block");
       document.body.append(block);
       block.style.height = height;
       block.style.width = width;
       block.style.background = bg;
       block.style.fontSize = fontSize;
-      block.style.position = 'absolute';
-      block.innerHTML = 'Привет';
-      document.addEventListener('keydown', function(e) {
-        const key = e.key;
-        if(key === 'ArrowRight'){
-          count += 10;
-          block.style.background = 'green';
-          block.style.left = count + 'px';
+      block.style.position = "absolute";
+      block.innerHTML = "Привет";
+      document.addEventListener("keydown", function (e) {
+        let cs = window.getComputedStyle(block);
+        let left = parseInt(cs.marginLeft);
+        let top = parseInt(cs.marginTop);
+
+        switch (e.key) {
+          case "ArrowLeft": // если нажата клавиша влево
+            if (left > 0) block.style.marginLeft = left - 10 + "px";
+            break;
+          case "ArrowTop": // если нажата клавиша вверх
+            if (top > 0) block.style.marginTop = top - 10 + "px";
+            break;
+          case "ArrowRight": // если нажата клавиша вправо
+            if (left < document.documentElement.clientWidth - 100)
+            block.style.marginLeft = left + 10 + "px";
+            break;
+          case "ArrowDown": // если нажата клавиша вниз
+            if (top < document.documentElement.clientHeight - 100)
+            block.style.marginTop = top + 10 + "px";
+            break;
         }
-        else if(key === 'ArrowLeft'){
-          count -= 10;
-          block.style.background = 'blue';
-          block.style.left = count + 'px';
-        } else if(key === 'ArrowDown'){
-          count += 10;
-          block.style.background = 'yellow';
-          block.style.top = count + 'px';
-        }else if(key === 'ArrowUp'){
-          count -= 10;
-          block.style.background = 'grey';
-          block.style.top = count + 'px';
-        }
-      })
-    }else if(selector === '#best'){
-       let paragraph = document.createElement('p');
-       paragraph.id = '#best';
-       document.body.append(paragraph);
-       paragraph.style.height = height;
-       paragraph.style.width = width;
-       paragraph.style.background = bg;
-       paragraph.style.fontSize = fontSize;
-       paragraph.innerHTML = 'Как дела';
+      });
+    } else if (selector === "#best") {
+      let paragraph = document.createElement("p");
+      paragraph.id = "#best";
+      document.body.append(paragraph);
+      paragraph.style.height = height;
+      paragraph.style.width = width;
+      paragraph.style.background = bg;
+      paragraph.style.fontSize = fontSize;
+      paragraph.innerHTML = "Как дела";
     }
   };
-  
-
 }
-let domElement = new DomElement('.block', '100px', '100px', 'blue', '14px');
+let domElement = new DomElement(".block", "100px", "100px", "blue", "14px");
 // let paragraph = new DomElement('#best', '100px', '100px', 'blue', '14px');
 DomElement.prototype.element();
